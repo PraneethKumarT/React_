c9268e43fb11a7b96ae76409a0c5476e810f13cd
Praneeth-Kumar_T <praneeth.kumt@gmail.com>
2020-09-29 22:13:48 +0530

    Components commit, week 2

---
+++ confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap'
+
+
+class DishDetail extends Component {
+    
+    constructor(props) {
+        super (props);
+    }
+
+
+    renderComments(comments) {
+
+        const commentss = comments.map(comment => {
+            return (
+                <li key = {comment.id} >
+                    <p>{comment.comment}</p>
+                    <p>-- {comment.author} ,
+                    {new Intl.DateTimeFormat("en-BR", {
+                        year: "numeric",
+                         month: "long",
+                         day: "2-digit"
+                          }).format(new Date(Date.parse((comment.date))))}
+                    </p>
+                </li>
+            )
+
+        })
+        return (
+            <div className ="col-12 col-md-5 m-1">
+                <h4>
+                    Comments
+                </h4>
+                <ul className='list-unstyled'>
+                    {commentss}
+                </ul>
+            </div>
+        )
+    
+    }
+
+
+    renderDish(dish){
+        if (dish != null){
+            return (
+                <div className =  "col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg width = "100%"  src={dish.image} alt = {dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description} </CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+            );
+        }
+        else
+            return(
+            <div></div>
+            );
+    }
+
+
+    render() {
+        const dish = this.props.dish
+        if (dish == null)
+            return (<div></div>)
+        const id = this.renderDish(dish)
+        const commentDish = this.renderComments(dish.comments)
+        return(
+            <div className = "container">
+                <div className = "row" >
+                       {id} 
+                       {commentDish}
+                </div>
+            </div>
+        )
+    }
+}
+export default DishDetail;
