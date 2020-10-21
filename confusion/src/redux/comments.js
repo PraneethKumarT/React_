6d89f2131ddbdb4d4b7b35038578aea8e7cae5c2
Praneeth-Kumar_T <praneeth.kumt@gmail.com>
2020-10-21 19:42:32 +0530

    Combining reducers

---
+++ confusion/src/redux/comments.js
@@ -0,0 +1,8 @@
+import {COMMENTS} from '../shared/comments';
+
+export const Comments = (state = COMMENTS, action0 => {
+    switch(action.type){
+        default:
+            return state;
+    }
+})
